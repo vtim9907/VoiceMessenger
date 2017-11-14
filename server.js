871bbc2c8d43836bdd915793593d26d8cb4ee5e3
vtim99077 <vtim99077@gmail.com>
2017-11-15 00:26:36 +0800

    Add both http and https version

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 var express = require('express');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
 var app = express();
-var port = 8086;
+var port = 9907;
+
 var bodyparser = require('body-parser');
 var multer = require('multer');
 
-app.use(express.static(__dirname+'/public'));
+var options = {
+		ca: fs.readFileSync('./ssl/ca_bundle.crt'),
+		key: fs.readFileSync('./ssl/private.key'),
+		cert: fs.readFileSync('./ssl/certificate.crt')
+		//requestCert: false,
+		//rejectUnauthorized: false
+};
 
+http.createServer(app).listen(9908);
+https.createServer(options, app).listen(port);
+
+app.use(express.static(__dirname+'/public'));
+//app.listen(port);
+//var server = https.createServer(options, app).listen(port, function(){
+//  console.log("server started");
+//});  
 /*
 app.post('blob', (req, res) => {
   req.pipe(fs.createWriteStream('public/myFile.wav'))
@@ -28,5 +46,3 @@ app.post('/mp3',mp3.single('mp3'),function(req,res){
 	res.sendStatus(200);
 });
 
-
-app.listen(port);
