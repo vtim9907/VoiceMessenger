5cb57b0ce891622b7d97e687e617dd0359806dec
q5988310 <32133054+q5988310@users.noreply.github.com>
2017-11-27 22:52:55 +0800

    改

---
+++ public/assets/card/card.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+  var card = Vue.extend({
+    template: '#card',
+        data: function(){
+              return {               
+                name:'王小明'
+              };
+        } 
+  })	
+}
