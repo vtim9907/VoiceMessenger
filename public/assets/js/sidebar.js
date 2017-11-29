0225e902527f0220777965e72a44b773b7d54bc5
yichung279 <ql3895619@gmail.com>
2017-11-29 18:17:58 +0800

    sidebar button

--- public/assets/js/sidebar.js
+++ public/assets/js/sidebar.js
@@ -1,45 +1,60 @@
- var sidebar=new Vue({
+var bus = new Vue();//用於事件傳遞)
+var temp=''
+var sidebar=new Vue({
    el:"#sidebar",
    data:{
-           
-     styleObject:'',
+     styleObject:{
+       position:'fixed',
+       display:'flex',
+       height:'100vh',
+       width:'250px'   
+     },
+     List:["好友","聊天","抽卡","錄音"]
+
    },
    methods:{
      response:function(){
        if(window.innerWidth>768){
-         this.styleObject={
-           position:'fixed',
-           display:'flex',
-           height:'100vh',
-           width:'250px'
-         }
+         this.styleObject.position='fixed'
+         this.styleObject.display='flex'
+         this.styleObject.height='100vh'
+         this.styleObject.width='250px'
+         
+       }else{
+         this.styleObject=''
+       }
+     },
+    show:function(i){
+       console.log(i);  
+       bus.$emit('PageChange',i);
+     }
+   } 
+})
+var mainPage=new Vue({
+   el:"#mainPage",
+   data:{ 
+     Pageshown:"",
+     styleObject:{  
+       margin:'',
+     }
+
+   },
+   methods:{
+     response:function(){
+       if(window.innerWidth>768){
+         this.styleObject.margin='0px 0px 0px 250px'
        }else{
          this.styleObject=''
        }
      }
-   } 
-})
-
- var mainPage=new Vue({
-   el:"#mainPage",
-   data:{           
-     styleObject:'',
    },
-   methods:{
-     response:function(){
-       if(window.innerWidth>768){
-         this.styleObject={
-           margin:'0px 0px 0px 250px', 
-         }
-       }else{
-         this.styleObject={
-         
-         }
-       }
-     }
-   } 
+   created: function(){
+    var a = this;
+      bus.$on('PageChange',function(i){
+        a.Pageshown=i;
+      } )
+   }
 })
-
 
 window.addEventListener("load",()=>{
  sidebar.response();
