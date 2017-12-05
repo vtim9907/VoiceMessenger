025d59f3993875b2350088d6f032513f9afda197
yichung279 <ql3895619@gmail.com>
2017-12-05 17:26:48 +0800

    friends with db(not test yet)

--- public/assets/friends/friends.js
+++ public/assets/friends/friends.js
@@ -2,12 +2,29 @@ var friends=Vue.extend({
   template : "#friends",
       data : function(){
         return {
-          lists:[{name:'aaa',imgurl:'https://goo.gl/eKdiuU'},
-                 {name:'bbb',imgurl:'https://goo.gl/eKdiuU'},
-                 {name:'ccc',imgurl:'https://goo.gl/eKdiuU'},
-                 {name:'ddd',imgurl:'https://goo.gl/eKdiuU'},
-                 {name:'eee',imgurl:'https://goo.gl/eKdiuU'}]
+          lists:[{nickname:'nickname1',photoPath:'https://goo.gl/eKdiuU'},
+                 {nickname:'nickname2',photoPath:'https://goo.gl/eKdiuU'},
+                 {nickname:'nickname3',photoPath:'https://goo.gl/eKdiuU'},
+                 {nickname:'nickname4',photoPath:'https://goo.gl/eKdiuU'},
+                 {nickname:'nickname5',photoPath:'https://goo.gl/eKdiuU'}]
         } ;
+      },
+      mounted() {
+        let self = this;
+        console.log("friends mounted");
+        $.ajax({
+            method: 'POST',
+            url: './getFriend',
+            dataType: 'json',
+            success: function(data) {
+              self.list=data.friendArray;
+            },
+            error: function() {
+                console.log("Error occured,friends.js");
+            }
+        })
+    }  
+      
       }
 })
 
