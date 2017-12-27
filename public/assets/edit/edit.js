03f5e79869173f42b3e0dd2f0a3e57bfe3ef6dc1
Vtim <vtim99077@gmail.com>
2017-12-27 10:47:26 +0800

    Modify model of user profile & beautify 'edit page'

--- public/assets/edit/edit.js
+++ public/assets/edit/edit.js
@@ -2,7 +2,27 @@ var edit = Vue.extend({
     template: "#edit",
     data: function () {
         return {
+            age: 20,
+            name: '',
+            gender: 0
         };
+    },
+    created() {
+        let self = this;
+        console.log("card created");
+        $.ajax({
+            method: 'POST',
+            url: './getProfile',
+            dataType: 'json',
+            success: function (data) {
+                self.name = data.name;
+                self.age = data.age;
+                self.gender = data.gender;
+            },
+            error: function () {
+                console.log("Error occured");
+            }
+        })
     }
 })
 
